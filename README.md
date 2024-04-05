c39838c99a871e6d7fd5e6784229a3b3532e0ebf
qwerzl <me@qwerzl.me>
2024-04-05 19:42:26 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# OpenBionic
+
+Built on top of [text-vide](https://github.com/Gumball12/text-vide), we aim to provide a free and opensource [Bionic Reading](https://bionic-reading.com/) alternative.
---
+++ components/Nav.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="w-screen border-b p-4">
+  <div class="text-3xl">
+    <b>Op</b>en<b>Bi</b>onic
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
